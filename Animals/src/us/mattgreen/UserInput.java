a29dd9a266fb8feecfdf0e0b34ab5edb426d5058
jayer1 <jayer1@my.wctc.edu>
2019-01-30 08:00:21 -0600

    Animals project

---
+++ Animals/src/us/mattgreen/UserInput.java
@@ -0,0 +1,20 @@
+package us.mattgreen;
+import java.util.Scanner;
+
+public class UserInput {
+    Scanner keyboard = new Scanner(System.in);
+    public UserInput(ArrayList arr){
+
+
+    }
+    (
+}ArrayList arr){
+
+
+    System.out.print("");
+
+        {
+        super(name);
+        this.mousesKilled = mousesKilled;
+        }
+}
